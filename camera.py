61942d436d95cbbbbe635a1ba63e3df3b0a0b277
BulmenI <74928947+BulmenI@users.noreply.github.com>
2025-01-20 17:43:46 +0500

    adds some logic

--- camera.py
+++ camera.py
@@ -7,3 +7,21 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 # Загрузка HOG дескриптора для туловища
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+video_capture = cv2.VideoCapture(0)
+
+human_count = 0
+show_count = False 
+display_count = 0 
+danger_activated = False 
+
+while True:
+    # Чтение кадра и преобразование в оттенки серого
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Обнаружение лиц
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Обнаружение туловища
+    (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
