97f7d3994c9e507466d2875a11a123873c4d4ac6
BulmenI <74928947+BulmenI@users.noreply.github.com>
2025-01-20 17:39:14 +0500

    Adds AI connections

--- camera.py
+++ camera.py
@@ -1,2 +1,9 @@
 import cv2
 import numpy as np
+
+# Загрузка каскадного классификатора для лиц
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Загрузка HOG дескриптора для туловища
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
